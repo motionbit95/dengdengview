55b04023cd3762718f6129a029125c3652ea1a5d
motionbit95 <motionbit.dev@gmail.com>
2024-04-24 15:27:12 +0900

    ✨ Feat : 웹 크롤링 예시

---
+++ src/Component/BlogCrawler.js
@@ -0,0 +1,47 @@
+// src/components/BlogCrawler.js
+import React, { useState } from "react";
+import axios from "axios";
+
+function BlogCrawler() {
+  const [url, setUrl] = useState("");
+  const [blogData, setBlogData] = useState(null);
+  const apiUrl = "http://localhost:3000/crawl";
+
+  const handleUrlChange = (e) => {
+    setUrl(e.target.value);
+  };
+
+  const fetchData = () => {
+    axios
+      .get(apiUrl + encodeURIComponent(url))
+      .then((response) => {
+        setBlogData(response.data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={url}
+        onChange={handleUrlChange}
+        placeholder="네이버 블로그 글 URL을 입력하세요"
+      />
+      <button onClick={fetchData}>불러오기</button>
+      {blogData && (
+        <div>
+          <img src={blogData.image} alt="이미지" />
+          <h2>{blogData.title}</h2>
+          <p>{blogData.content}</p>
+          <p>작성날짜: {blogData.date}</p>
+          <p>작성자: {blogData.author}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default BlogCrawler;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,10 +2,12 @@ import React from "react";
 import Campain from "./Campain";
 import { Stack } from "@chakra-ui/react";
 import { Tabs1 } from "../Application/Tabs/Tabs1";
+import BlogCrawler from "../Component/BlogCrawler";
 
 function Home(props) {
   return (
     <Stack>
+      <BlogCrawler />
       <Tabs1 />
       <Campain />
     </Stack>
